36d40571913eaf4381dce85f9f7690c95006ea90
Lauro Enrique Huanca Solis <lhuancas@emeal.nttdata.com>
2022-03-20 12:40:06 -0500

    servicio de registro de microservicios

---
+++ src/test/java/com/ehsproy/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehsproy.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
